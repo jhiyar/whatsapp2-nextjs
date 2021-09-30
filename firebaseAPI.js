c7d93dca64932268b18a142049c3c1c1e9decfa7
Jiar Ebrahimpour <61942835+jhiyar@users.noreply.github.com>
2021-09-30 07:40:39 -0700

    Update firebaseAPI.js

--- firebaseAPI.js
+++ firebaseAPI.js
@@ -1,10 +1,5 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyD3FzSz0555IFlZx8NQ4jxFPogwtKtRAf4",
-    authDomain: "whatsapp2-d0d42.firebaseapp.com",
-    projectId: "whatsapp2-d0d42",
-    storageBucket: "whatsapp2-d0d42.appspot.com",
-    messagingSenderId: "232576900207",
-    appId: "1:232576900207:web:508fb828ba18c598be019a"
+   
 };
 
 export default firebaseConfig;
